8b21cc86b70fcc029e7d6e9b47ab8391dd535d4e
mdeias <mdeias@icloud.com>
2021-11-12 13:49:06 +0100

    ciclo v-for lista

--- js/script.js
+++ js/script.js
@@ -3,11 +3,32 @@ const app = new Vue({
     el: '#app',
 
     data: {
-
+        listaToDo: [
+            {
+                text: 'Cercare una destinazione',
+                done: false
+            },
+            {
+                text: 'Prenotare il volo',
+                done: false
+            },
+            {
+                text: "Chiamare l'albergo",
+                done: false
+            },
+            {
+                text: 'Preparare le valige',
+                done: false
+            },
+            {
+                text: "Rifornire l'auto",
+                done: false
+            },
+        ]
     },
 
     methods:{
-        
+
     }
 
 })
